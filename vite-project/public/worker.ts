2b61e2aaac48af7d13af13978c805e59c9f2a9c6
kotobukid <kotobuki@daqua.jp>
2023-05-12 01:45:57 +0900

    フロント側(vite)に配置してあるworker.tsファイルをworker.jsに変換したかった(viteではバンドルしかしないので)ため、express用のtscのwatch対象ファイルに強引に加えたところ、意図通りに動作した

---
+++ vite-project/public/worker.ts
@@ -0,0 +1,8 @@
+// @ts-ignore
+self.addEventListener('message', (info: MessageEvent<{type: string, payload: any}>) => {
+    const type = info.type;
+    console.log(type);
+    console.log(info.data);
+
+    self.postMessage('return hello');
+});
--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
+
+// Web Workerの作成と起動
+const worker = new Worker(new URL('/worker.js', import.meta.url), {type: 'module'})
+
+// Workerからメッセージを受け取る
+worker.onmessage = (event) => {
+    console.log('Message received from worker', event.data)
+}
+
+// Workerにメッセージを送信
+worker.postMessage({type: 'hello', message: 'Hello, worker!'})
 
 createApp(App).mount('#app')
