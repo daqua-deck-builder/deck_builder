f6d682f9de466777e83266ce8e755c26a29d788e
kotobukid <kotobuki@daqua.jp>
2023-05-13 01:30:23 +0900

    build時にファイルを見つけられないという状態だったため、一旦戻す (警告は出ない場合もある？)

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,16 +1,10 @@
-import {createApp} from 'vue'
+import {createApp, provide} from 'vue'
 import './style.css'
 import App from './App.vue'
 
 // Web Workerの作成と起動
-const worker = new Worker(new URL('../worker.js', import.meta.url), {type: 'module'})
+const worker = new Worker(new URL('/worker.js', import.meta.url), {type: 'module'})
 
-// Workerからメッセージを受け取る
-worker.onmessage = (event) => {
-    console.log('Message received from worker', event.data)
-}
-
-// Workerにメッセージを送信
-worker.postMessage({type: 'hello', message: 'Hello, worker!'})
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.provide('worker', worker);
+app.mount('#app');
